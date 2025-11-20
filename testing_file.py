14681072030920df6940ca65f1ea8cb97de9de23
Gabe <gabejacallaghan@gmail.com>
2025-11-20 11:24:24 -0500

    added stuff

--- testing_file.py
+++ testing_file.py
@@ -1,8 +1,12 @@
+import time
 
 start_time = time.time()
 
 
-
+#GRABBING END TIME
 end_time = time.time()
 elapsed_time = end_time - start_time
 elapsed_minutes = elapsed_time / 60
+
+#CALCULATING WORD COUNT
+wpm = word_count / elapsed_minutes
