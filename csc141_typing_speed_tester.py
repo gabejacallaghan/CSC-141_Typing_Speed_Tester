35c90b9a028e7142cf4df427b355cdbda5ae22cf
Gabe <gabejacallaghan@gmail.com>
2025-11-20 11:18:14 -0500

    moved to text file

--- csc141_typing_speed_tester.py
+++ csc141_typing_speed_tester.py
@@ -1,12 +1,2 @@
 import time
 import tkinter as tk
-
-start_time = time.time()
-
-
-
-end_time = time.time()
-
-
-
-elapsed_time = end_time - start_time
---
+++ testing_file.py
@@ -0,0 +1,8 @@
+
+start_time = time.time()
+
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+elapsed_minutes = elapsed_time / 60
